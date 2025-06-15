339b7f22387b4241d4ccc846d0209c97eaa2e300
junglejim98 <bloodamerika@gmail.com>
2025-06-15 12:13:59 +0300

    start codding

--- src/pages/amino_acids.tsx
+++ src/pages/amino_acids.tsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import styles from "./amino_acids.module.scss"
 
 export default function aminoAcids () {
-    const [acids, setAcids] = useState({first: '', second: ''});
+    const [acids, setAcids] = useState<{first: string; second: string}>({first: '', second: ''});
 
-    
+    const handleChange = (name: string, value: string) => {
+      setAcids(prev => ({...prev, [name]: value}))
+    }
+
 }
