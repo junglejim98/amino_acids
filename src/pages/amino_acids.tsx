4f42538face1bb702a926747f42b0ac411061d10
junglejim98 <bloodamerika@gmail.com>
2025-06-15 11:55:10 +0300

    deployed project

---
+++ src/pages/amino_acids.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import styles from "./amino_acids.module.scss"
+
+export default function aminoAcids () {
+    const [acids, setAcids] = useState({first: '', second: ''});
+
+    
+}
