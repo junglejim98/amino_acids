b93095b09b7431e011927a9afa47982cd04608f0
Lysyonok Gregory <bloodamerika@gmail.com>
2025-06-16 17:45:24 +0300

    inputs created, pages test

--- src/pages/amino_acids.tsx
+++ src/pages/amino_acids.tsx
@@ -1,11 +1,42 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import styles from "./amino_acids.module.scss"
 
-export default function aminoAcids () {
+export default function AminoAcids () {
     const [acids, setAcids] = useState<{first: string; second: string}>({first: '', second: ''});
+    const [attempted, setAttempt] = useState<boolean>(false);
+    const isSameLength = acids.first.length === acids.second.length;
+    const needLiveCheck = !isSameLength && attempted;
 
-    const handleChange = (name: string, value: string) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const {name, value} = e.target;
       setAcids(prev => ({...prev, [name]: value}))
     }
+    
+    function onSubmit (e:FormEvent<HTMLFormElement>) {
+      e.preventDefault();
+      if(!isSameLength)
+        setAttempt(true);
+    }
 
+    
+    return (
+      <div>
+          <form onSubmit={onSubmit}>
+                <label>
+                  Первая последовательность
+                <input type='text' name = 'first' value={acids.first} onChange={handleChange}/>
+                </label>
+                <label>
+                  Вторая последовательность
+                <input type='text' name = 'second' value={acids.second} onChange={handleChange}/>
+                </label>
+                <button disabled={!isSameLength || !acids.first || needLiveCheck}>Проверить кислоты</button>
+                  { !isSameLength && attempted && <p>Длины последовательностей не совпадают</p>}
+          </form> 
+
+          <div>
+
+          </div>
+     </div>
+)
 }
